b001918dd14d0f6b257a44ba8b002b240b36195c
blockchainluffy <jainpunit1997@gmail.com>
2025-06-25 16:42:43 +0530

    fix: add staker-scripts@v0.1.0 with exlicit 8545 & 4000 port check

--- shutter/scripts/configure_keyper.sh
+++ shutter/scripts/configure_keyper.sh
@@ -16,7 +16,7 @@ PATH=$NODE_HOME/bin:$PATH
 function test_ethereum_url() {
     # FIXME: This is a workaround for the issue with the staker-scripts@v0.1.1 not setting get_execution_ws_url_from_global_env correctly in the environment variables.
     # Git Issue: https://github.com/dappnode/staker-package-scripts/issues/11
-    export SHUTTER_GNOSIS_NODE_ETHEREUMURL=${ETHEREUM_WS:-ws://execution.${NETWORK}.dncore.dappnode:8546}
+    export SHUTTER_GNOSIS_NODE_ETHEREUMURL=${ETHEREUM_WS:-get_execution_ws_url_from_global_env ${NETWORK} ${SUPPORTED_NETWORKS}}
     RESULT=$(wscat -c "$SHUTTER_GNOSIS_NODE_ETHEREUMURL" -x '{"jsonrpc": "2.0", "method": "eth_syncing", "params": [], "id": 1}')
     if [[ $RESULT =~ '"id":1' ]]; then return 0; else
         export SHUTTER_GNOSIS_NODE_ETHEREUMURL=ws://execution.${NETWORK}.dncore.dappnode:8545
@@ -31,12 +31,14 @@ function test_ethereum_url() {
 
 function test_beacon_url() {
     export SHUTTER_BEACONAPIURL=${BEACON_HTTP:-$(get_beacon_api_url_from_global_env "$NETWORK" "$SUPPORTED_NETWORKS")}
-    echo "[DEBUG | configure] SHUTTER_BEACONAPIURL is ${SHUTTER_BEACONAPIURL}"
     RESULT=$(curl -X GET "${SHUTTER_BEACONAPIURL}/eth/v1/beacon/genesis" -H "Accept: application/json")
     if [[ $RESULT =~ '"genesis_time"' ]]; then return 0; else
-        echo "Could not find DAppNode Beacon API url for this package!"
-        echo "Please configure 'BEACON_HTTP' to point to an applicable HTTP API service."
-        exit 1;
+        export SHUTTER_BEACONAPIURL=http://beacon-chain.${NETWORK}.dncore.dappnode:4000
+        if [[ $RESULT =~ '"genesis_time"' ]]; then return 0; else
+            echo "Could not find DAppNode Beacon API url for this package!"
+            echo "Please configure 'BEACON_HTTP' to point to an applicable HTTP API service."
+            exit 1;
+        fi
     fi
 }
 
@@ -53,6 +55,7 @@ export SHUTTER_P2P_ADVERTISEADDRESSES="[\"/ip4/${_DAPPNODE_GLOBAL_PUBLIC_IP}/tcp
 
 
 test_beacon_url
+echo "[DEBUG | configure] SHUTTER_BEACONAPIURL is ${SHUTTER_BEACONAPIURL}"
 
 export SHUTTER_GNOSIS_NODE_CONTRACTSURL=http://execution.gnosis.dncore.dappnode:8545
 
